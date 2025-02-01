a410f92b380c8db205692a1550495307fde299a5
Abhishek <53704940+abhi526691@users.noreply.github.com>
2025-01-31 23:29:03 -0500

    updated env for frontend

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
-export const API_BASE_URL = 'http://localhost:8000';
+// export const API_BASE_URL = 'http://localhost:8000';
+// const API_BASE_URL = process.env.REACT_APP_API_URL;
+// console.log("API URL:", API_BASE_URL);
+const API_BASE_URL = import.meta.env.VITE_API_URL;
+console.log("API URL:", API_BASE_URL);
+
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
